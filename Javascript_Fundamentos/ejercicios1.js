dc5aa029c4c33608117aad9fe28ff26e86e01647
andie_farias <andie.glezfarias@gmail.com>
2023-01-04 19:54:50 -0600

    ejercicios de javascript 1

---
+++ Javascript_Fundamentos/ejercicios1.js
@@ -0,0 +1,15 @@
+// Ejercicio 1
+alert("Hello World");
+// Ejercicio 2 
+document.write("Hello World\n");
+// Ejercicio 3
+document.write(3+5);
+// Ejercicio 4
+let person = prompt("Escribe tu nombre")
+    document.write("\nHola "+ person);
+// Ejercicio 5
+let n1 = prompt("Escribe un número")
+let n2 = prompt("Escribe otro número")
+    document.write("\n El Resultado es " + (parseInt(n1) + parseInt(n2)));
+// let suma=parseInt(n1) + parseInt(n2);
+//     document.write("\n El resultado es " + suma);
