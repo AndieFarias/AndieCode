b5f6ecb925141656592667c786c72433cac7fc1a
andie_farias <andie.glezfarias@gmail.com>
2023-01-13 17:31:08 -0600

    ejercicios de js y objetos

--- Javascript_Fundamentos/ejercicios1.js
+++ Javascript_Fundamentos/ejercicios1.js
@@ -1,15 +1,74 @@
 // Ejercicio 1
-alert("Hello World");
-// Ejercicio 2 
-document.write("Hello World\n");
-// Ejercicio 3
-document.write(3+5);
-// Ejercicio 4
-let person = prompt("Escribe tu nombre")
-    document.write("\nHola "+ person);
-// Ejercicio 5
-let n1 = prompt("Escribe un número")
-let n2 = prompt("Escribe otro número")
-    document.write("\n El Resultado es " + (parseInt(n1) + parseInt(n2)));
-// let suma=parseInt(n1) + parseInt(n2);
-//     document.write("\n El resultado es " + suma);
+// alert("Hello World");
+
+// // Ejercicio 2 
+// document.write("Hello World\n");
+
+// // Ejercicio 3
+// document.write(3+5);
+
+// // Ejercicio 4
+// let person = prompt("Escribe tu nombre");
+//     document.write("\nHola "+ person);
+
+// // Ejercicio 5
+// let n1 = prompt("Escribe un número");
+// let n2 = prompt("Escribe otro número");
+//     document.write("\n El Resultado es " + (parseInt(n1) + parseInt(n2)));
+
+// Ejercicio 6: Escribe un programa que pida dos números y escriba en la pantalla cuál es el mayor
+
+// let x = Number (prompt ("Escribe un número"));
+// let y = Number (prompt ("Escribe otro número"));
+//     if (x > y) {
+//         document.write (x);
+//     } else {
+//         document.write (y);
+//     }
+
+// Ejercicio 7: Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+// let a = Number(prompt ("Escribe un número"));
+// let b = Number(prompt ("Escribe otro número"));
+// let c = Number(prompt ("Escribe otro número más"));
+//     if (a>b && a>c) {
+//         document.write (a);
+//     } else if (b>c) {
+//         document.write (b);
+//     } else {
+//         document.write (c);
+//     }
+
+// Ejercicio 8: Escribe un programa que pida un número y diga si es divisible entre 2
+
+// let cantidad = Number(prompt ("Escribe cualquier número y veamos si se divide entre 2"));
+    // if (cantidad % 2 === 0) {
+    //     alert("El número es divisible entre 2");
+    // } else {
+    //     alert("El número no es múltiplo de 2");
+    // }
+
+
+
+
+// // Ejercicio 9: Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
+//  function count() {
+//      let letraA="a", contar=0;
+//      let frase= prompt("Escribe una frase corta");
+//      frase=frase.toLowerCase ();
+//      for (let i=0;i<frase.length; i++) {
+//          if (frase [i]==letraA) {
+//              contar++
+//          }
+//      }
+//      alert("La cantidad de a's es: " +contar);
+//  }
+// count()
+
+// // Ejercicio 10: Escribe un programa que pida una frase y escriba las vocales que aparecen en ella.
+
+
+// // BONUS: Escribe un programa que pida una frase y escriba cuántas de las letras que tiene son vocales.
+
+
+
---
+++ Javascript_Fundamentos/objetos.js
@@ -0,0 +1,16 @@
+//Objeto sin propiedades
+
+/* let persona= {}
+console.log(persona)
+ */
+
+//Objeto con propiedades
+let persona= {
+    nombre:"Andie",
+    apellido:"Farias",
+    edad:31,
+    estudios:["Licenciatura", "Maestria"],
+    hijos:false
+}
+
+console.log(persona.apellido);
