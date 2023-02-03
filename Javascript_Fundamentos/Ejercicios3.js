a05c06c07b041675958ae8e8e9a7d8b0f262c62e
andie_farias <andie.glezfarias@gmail.com>
2023-02-02 18:29:35 -0600

    proyecto formulario

---
+++ Javascript_Fundamentos/Ejercicios3.js
@@ -0,0 +1,23 @@
+// Pide la edad y si es mayor a 18 ya puede conducir
+let edadUsuario = prompt("Escribe tu edad");
+if (Number(18)==18) {
+    if(edadUsuario > 18){
+          document.write("Puedes conducir");
+    }
+}
+else{
+    alert("Introduce un número válido");
+}
+
+//Pide una nota (número). Muestra la calificación según la nota
+
+
+//Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión
+
+
+//Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo. Al salir con “cancelar” deberá indicarse la suma total de los números introducido
+
+
+//Realizar una página con un script que calcule el valor de la letra de un número de DNI 
+
+
